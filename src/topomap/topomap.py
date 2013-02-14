7971087d195f9ded2021340dbb96b96fdcbf03c6
Martijn Meijers <b.m.meijers@tudelft.nl>
2013-02-14 10:44:26 +0100

    Added clipped reconstruction to TopoMap

--- src/topomap/topomap.py
+++ src/topomap/topomap.py
@@ -208,9 +208,124 @@ class LoopFactory(object):
                         if edge is start:
                             break
 
-#        for face in topomap.faces.itervalues():
-#            try:
-#                face.multigeometry()
-#            except:
-#                print face
-#                raise
+    @classmethod
+    def find_clipped_loops(cls, topomap, half_edges = None):
+        """Find all Loop objects for a TopoMap 
+        and adds them to the Face they belong to
+        
+        The edges of the TopoMap can be clipped and should have a field in
+        their attrs-dict `clipped' that evaluates to True
+        """
+        #
+        # Make 3 passes over the edges
+        # 1. reconstruct loops fully inside
+        # 2. reconstruct clipped loops, starting on a edge that is not a border segment
+        # 3. reconstruct remaining loops (consisting of border segments only)
+        for item in topomap.half_edges.itervalues():
+            for edge in (item, item.twin):
+                if edge.label == VISITED:
+                    continue
+                elif 'clipped' in edge.attrs and edge.attrs['clipped']:
+                    continue
+                else:
+                    start = edge
+                    loop = Loop(start)
+                    start.face.loops.append(loop)
+                    guard = 0
+                    while True:
+                        guard += 1
+                        if guard > 500000:
+                            raise Exception('Too much iteration for {0}, started at {1}'.format(start.face, start))
+                        edge.label = VISITED
+                        edge.loop = loop
+                        if 'clipped' in edge.attrs and edge.attrs['clipped']:
+                            edge.face = start.face
+                        try:
+                            assert edge.face is start.face, "{0}, reconstructing: {1}".format(edge, start.face)
+                        except:
+                            print "ERROR: {0}".format(edge)
+                            print "... reconstructing: {0}".format(start.face)
+                            print "...", edge, edge.face, start.face
+                            print ""
+                            raise
+                        edge = edge.next
+                        if edge is start:
+                            break
+        for item in topomap.half_edges.itervalues():
+            for edge in (item, item.twin):
+                if edge.label == VISITED:
+                    continue
+                elif edge.right_face is edge.left_face:
+                    continue
+                else:
+                    start = edge
+                    loop = Loop(start)
+                    start.face.loops.append(loop)
+                    guard = 0
+                    while True:
+                        guard += 1
+                        if guard > 500000:
+                            raise Exception('Too much iteration for {0}, started at {1}'.format(start.face, start))
+                        edge.label = VISITED
+                        edge.loop = loop
+                        if 'clipped' in edge.attrs and edge.attrs['clipped']:
+                            edge.face = start.face
+                        try:
+                            assert edge.face is start.face, "{0}, reconstructing: {1}".format(edge, start.face)
+                        except:
+                            print "ERROR: {0}".format(edge)
+                            print "... reconstructing: {0}".format(start.face)
+                            print "...", edge, edge.face, start.face
+                            print ""
+                            raise
+                        edge = edge.next
+                        if edge is start:
+                            break
+        for item in topomap.half_edges.itervalues():
+            for edge in (item, item.twin):
+                if edge.label == VISITED:
+                    continue
+                else:
+                    start = edge
+                    loop = Loop(start)
+                    start.face.loops.append(loop)
+                    guard = 0
+                    while True:
+                        guard += 1
+                        if guard > 500000:
+                            raise Exception('Too much iteration for {0}, started at {1}'.format(start.face, start))
+                        edge.label = VISITED
+                        edge.loop = loop
+                        if 'clipped' in edge.attrs and edge.attrs['clipped']:
+                            edge.face = start.face
+                        try:
+                            assert edge.face is start.face, "{0}, reconstructing: {1}".format(edge, start.face)
+                        except:
+                            print "ERROR: {0}".format(edge)
+                            print "... reconstructing: {0}".format(start.face)
+                            print "...", edge, edge.face, start.face
+                            print ""
+                            raise
+                        edge = edge.next
+                        if edge is start:
+                            break
+        fh = open('/tmp/tm.wkt', 'w')
+        fh.write('id;wkt;clipped\n')
+        to_remove = []
+        for face in topomap.faces.itervalues():
+            if not face.loops:
+                to_remove.append(face.id)
+                continue
+            if face.unbounded:
+                continue
+            try:
+                geoms = face.multigeometry()
+            except Exception, err:
+                print err
+                pass
+            if geoms:
+                for geom in geoms:
+                    fh.write("{};{};{}\n".format(face.id, geom, face.attrs['clipped']))
+        fh.close()
+        for remove in to_remove:
+            topomap.remove_face(remove)
