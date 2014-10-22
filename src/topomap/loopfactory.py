571e1cc36f58f699ea56e9466cb60571a62bd424
Radan Šuba <nadarabus@gmail.com>
2014-10-22 15:34:22 +0200

    code is simplier, no labels are used.

--- src/topomap/loopfactory.py
+++ src/topomap/loopfactory.py
@@ -10,47 +10,42 @@ VISITED = 1
 
 
 def find_loops_partly(tm):
-    """Finds loop objects, starting on half_edges labeled as 
-    INIT
-    """
+    """Finds loop objects, starting on half_edges without loop"""
     visit = set()
     for he in tm.half_edges.itervalues():
         for h in (he, he.twin):
-            if h.label == INIT:  
+            if h.loop is None:
                 visit.add(h)  
 #                
 #    with open(r"d:\A07_visit.wkt", "w") as fh:
 #        fh.write("id; label; geometry\n")
 #        for he in visit:
-#            fh.write("{H.id};{H.label};{H.geometry}\n".format(H=he))            
+#            fh.write("{H.id};{H.label};{H.geometry}\n".format(H=he)) 
+                       
     find_loops(tm, visit)
 
 def find_loops(topomap, half_edges = None):
     """Find all Loop objects for a TopoMap 
     and adds them to the Face they belong to"""
     if half_edges is None:
-        topomap.label_half_edges(INIT)
         half_edges = topomap.half_edges.itervalues()
 
     for item in half_edges:            
-#        print "find loop", item.id, item.face, item.label, "twin", item.twin.id, item.twin.face, item.twin.label
         for edge in (item, item.twin):
-#            print "edge", edge.id, edge.face, edge.label
-            if edge.label == VISITED:
+            if edge.loop is not None:
                 continue
             else:
                 start = edge
                 assert start.face.attrs['locked'] == False
-#                print "find loop, start", start.id
                 loop = Loop(start)
                 start.face.loops.append(loop)
                 guard = 0
                 while True:
-#                    print "find loop, he", edge.id, edge.face, edge.label
                     guard += 1
                     if guard > 500000:
                         raise Exception('Too much iteration for {0}, started at {1}'.format(start.face, start))
-                    edge.label = VISITED
+                    edge.label = VISITED #this guarantee that all edges after forming loop are set to VISITED
+                    assert edge.loop is None
                     edge.loop = loop
                     try:
                         assert edge.face is start.face, "{0}, reconstructing: {1}".format(edge, start.face)
@@ -63,9 +58,6 @@ def find_loops(topomap, half_edges = None):
                     edge = edge.next
                     if edge is start:
                         break
-#            print item
-#            print "edge", edge
-#            raw_input("press button")
 
 def find_clipped_loops(topomap, half_edges = None):
     """Find all Loop objects for a TopoMap 
